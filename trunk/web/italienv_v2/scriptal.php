5fd8145eadaf3511fe32e5191c2241b4740d312f
Damien.Gouteux@gmail.com <Damien.Gouteux@gmail.com@58e3cefd-5e55-0410-8322-4f36134d7f66>
2012-01-24 23:25:22 +0000

    italien v2

---
+++ trunk/web/italienv_v2/scriptal.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>Scriptal</title>
+        <link rel="stylesheet" type="text/css" href="web5.css" />
+    </head>
+    <body>
+    <table width="100%" height="100%"><tr height="10%"><td></td></tr><tr height="80%"><td align="center" valign="center">
+        <div class="main">
+        <span></span>
+        <table class="invisible"><tr><td align="left"><h1>Scriptal</h1><td align="right"><img src="leaf0010.png" height="60" width="60"/></td></tr></table>
+        <hr/>
+        <table class="inside">
+            <tr>
+                <td class="menu">
+                    <ul>
+                        <li><a href="index.php">News</a></li>
+                        <li><a href="beatrice.php">Beatrice</a></li>
+                        <li><a href="scriptal.php">Scriptal</a></li>
+                        <li><a href="about.php">A propos</a></li>
+                </td>
+                <td class="middle">
+                     <div class="invisible" align="center"><p class="date">22 Janvier 2012</p></div>
+                    <p class="text">Because as you know, there is nothing as good as pipo. I'm sure of it! Let me explain: Pipo is the sole force in the entire universe to be pipofull.</p>
+                    <p class="text">Nothing to say my dear!</p>
+                    <a href="http://www.google.com">Go back!</a>
+                </td>
+                <td class="left">
+                    <div class="invisible" style="border: 1px dotted black; background-color: #F9FAED;"> <!--EFEFEF-->
+                        <p class="rubrique">Dernières nouvelles</p>
+                        <ul class="rubrique">
+                            <li>>> <a href="">22 Janvier 2012</a></li>
+                        </ul>
+                    </div>
+                    <p class="text">Hello you! I'm here! Yes, right here! Welcome my old friend to this website. It's a test you know. Because I don't want to bother you with more, let's have some code!</p>
+                    <p class="code">&nbsp;<span class="k">if</span> a == 5 <span class="k">then</span><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;println(a)<br/><span class="k">&nbsp;end</span></p>
+                </td>
+            </tr>
+        </table>
+        </div>
+        <p class="footer">Damien Gouteux, 2012</p>
+    </td><tr width="10%"><td></td></tr></table>
+    </body>
+</html>
