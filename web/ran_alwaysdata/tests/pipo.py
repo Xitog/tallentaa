8f5e29f37eb11c4d88df80fa30b9cd014416a845
dgouteux@gmail.com <dgouteux@gmail.com@58e3cefd-5e55-0410-8322-4f36134d7f66>
2012-06-20 12:31:52 +0000

    Fin mise à jour web.

---
+++ web/ran_alwaysdata/tests/pipo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+ 
+# make file
+# chmod+x
+ 
+print "Content-type: text/html"
+print 
+print "<title>CGI 101</title>"
+print "<h1>A First CGI Example</h1>"
+print "<P>Hello, CGI World!</p>"
