19b9a4af796184aaeb9ad0a931536f38da7959fd
dgouteux@gmail.com <dgouteux@gmail.com@58e3cefd-5e55-0410-8322-4f36134d7f66>
2012-06-20 12:31:52 +0000

    Fin mise à jour web.

---
+++ trunk/web/ran_alwaysdata/tests/pipo.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+puts "Content-type: text/html"
+puts
+puts "<title>CGI 101</title>"
+puts "<h1>A First CGI Example</h1>"
+puts "<hr>"
+puts "<p>Hello, CGI World!</p>"
+puts "<br>"
+puts "<i>made with Ruby ;-)</i>"
