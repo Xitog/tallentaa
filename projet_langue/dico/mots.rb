9172458d2ad4b773780532b13e9b0bfc10ab0205
dgouteux@gmail.com <dgouteux@gmail.com@58e3cefd-5e55-0410-8322-4f36134d7f66>
2012-06-28 12:28:10 +0000

    DICO

--- projet_langue/dico/mots.rb
+++ projet_langue/dico/mots.rb
@@ -1,10 +1,8 @@
-#http://stackoverflow.com/questions/1000688/what-is-the-canonical-way-to-trim-a-string-in-ruby-without-creating-a-new-string
-
-f = File.new('base.data', 'r')
-lines = f.readlines()
-words = []
+#!/usr/bin/ruby
 
 class Mot
+    attr_reader :base, :kind, :cat
+    
     def initialize(base, kind, cat)
         @base = base
         @kind = kind
@@ -16,18 +14,27 @@ class Mot
     end
 end
 
-for l in lines do
-    part = l.split(':')
-    for p in part do
-        p.strip!
-    end
-    if part.length > 2 then
-        words << Mot.new(part[0], part[1], part[2])
+class Base
+    attr_reader :words
+    
+    def initialize()
+        f = File.new('base.data', 'r')
+        lines = f.readlines()
+        words = []
+        for l in lines do
+            part = l.split(':')
+            for p in part do
+                p.strip!
+            end
+            if part.length > 2 then
+                words << Mot.new(part[0], part[1], part[2])
+            end
+        end
+        @words = words
     end
 end
 
-for w in words do
-    puts w
-end
-
-puts words.length
+#for w in words do
+#    puts w
+#end
+#puts words.length
