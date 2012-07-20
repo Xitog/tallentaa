fac17250922a7fc8851bcc2519d95ddfa445e789
dgouteux@gmail.com <dgouteux@gmail.com@58e3cefd-5e55-0410-8322-4f36134d7f66>
2012-07-20 11:56:48 +0000

    Arbre et modif editor

--- trunk/projet_rts/python/simple_editor.py
+++ trunk/projet_rts/python/simple_editor.py
@@ -30,8 +30,20 @@ up = False
 left = False
 right = False
 
-tree = pygame.image.load('arbre.png').convert()
-tree.set_colorkey((255,0,255))
+apply = False
+content = [
+    ('none', 0, 0),
+    ('tree1', -48, -4*32),
+    ('tree2', -48, -4*32),
+    ('tree3', -48, -4*32),
+  ]
+actual_content = 1
+
+content_img = []
+for c in content:
+    i =  pygame.image.load(c[0]+'.png').convert()
+    i.set_colorkey((255,0,255))
+    content_img.append(i)
 
 while 1:
     #-------------------------------------------------------------------
@@ -54,9 +66,16 @@ while 1:
             elif event.key == K_UP: up = False
             elif event.key == K_LEFT: left = False
             elif event.key == K_RIGHT: right = False
+            elif event.key == K_SPACE: actual_content += 1
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                apply = True
         elif event.type == MOUSEBUTTONUP:
-            print 'line=', my32, 'column=', mx32, Y, X
-            if mx32 > 0 and mx32 < MAP_X and my32 > 0 and my32 < MAP_Y: print area[my32][mx32]
+            if event.button == 1:
+                apply = False
+            elif event.button == 3:
+                print 'line=', my32, 'column=', mx32, Y, X
+                if mx32 > 0 and mx32 < MAP_X and my32 > 0 and my32 < MAP_Y: print area[my32][mx32]
     
     #-------------------------------------------------------------------
     # Update
@@ -65,6 +84,9 @@ while 1:
     if down and Y < MAP_Y-MAX_Y: Y += 1
     if up and Y > 0: Y -= 1
     #if left or right or down or up: print X, Y
+    if apply:
+        if mx32 >= 0 and mx32 < MAP_X and my32 >= 0 and my32 < MAP_Y: area[my32][mx32] = actual_content
+        print 'apply=', actual_content
     
     #-------------------------------------------------------------------
     # Draw
@@ -75,10 +97,11 @@ while 1:
             r = area[yy+Y][xx+X]
             #except:
             #    print 'y=', Y, yy+Y, 'x=', X, xx+X
-            if r == 1:
+            if r > 0:
                 pygame.draw.rect(screen, Color(255, 0, 0, 128), (xx*32, yy*32, 32, 32), 1)
-                screen.blit(tree, (xx*32-48,yy*32-4*32))
+                screen.blit(content_img[r], (xx*32+content[r][1],yy*32+content[r][2]))
             else:
                 pygame.draw.rect(screen, Color(0, 255, 0, 128), (xx*32, yy*32, 32, 32), 1)
+    screen.blit(content_img[actual_content], (500, 400))
     pygame.display.flip()
     pygame.time.Clock().tick(60)
