bb90c0ceca5969e59ae0a9d00176ea9eacc58687
dgouteux@gmail.com <dgouteux@gmail.com@58e3cefd-5e55-0410-8322-4f36134d7f66>
2012-10-12 15:30:42 +0000

    2 windows. 1 application.

--- trunk/projet_rts/python/simple_editor.py
+++ trunk/projet_rts/python/simple_editor.py
@@ -2,12 +2,33 @@
 
 # v2: 45m: ajout du sol en plus des doodads. cycle. style objet. +20m ajout des entites.
 
+#-----------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------
+
 import pygame, sys, math, random
 from pygame.locals import *
 
+#-----------------------------------------------------------------------
+# Global Vars
+#-----------------------------------------------------------------------
+
+# mode of edition
+SELECT = 0
+GROUND = 1
+DOODAD = 2
+ENTITY = 3
+MODE_MAX = 4
+
+#-----------------------------------------------------------------------
+# Classes
+#-----------------------------------------------------------------------
+
 class Entity:
-    def __init__(self):
-        pass
+    def __init__(self, vie_max=100, vie=100):
+        self.angle = 1
+        self.vie = vie
+        self.vie_max = vie_max
 
 class Window:
     
@@ -33,19 +54,30 @@ class Menu(Window):
     def on_event(self, event):
         if event.type == MOUSEBUTTONUP:
             x,y = event.pos
-            print x,y
+            #print x,y
+            #print (x-100)/35
+            #print (y-500)/35
+            print (x-100)/35 + 19*((y-500)/35)
     
     def draw(self, surf):
         pygame.draw.rect(surf, self.background_color, (self.x, self.y, self.w, self.h), 1)
-        if self.editor.mode == 'ground':
+        
+        surf.blit(self.editor.content_img[self.editor.doodad_content], (500, 450))
+        if self.editor.ground_content == 0:
+            pygame.draw.rect(surf, (0, 0, 255), (400, 500, 32, 32), 0)
+        elif self.editor.ground_content == 1:
+            pygame.draw.rect(surf, (255, 0, 0), (400, 500, 32, 32), 0)
+        pygame.draw.circle(surf, (255, 255, 0), (300+16, 500+16), 16, 0)
+        
+        if self.editor.mode == GROUND:
             pygame.draw.rect(surf, (255, 255, 0), (390, 490, 128, 128), 1)
             pygame.draw.rect(surf, (255, 255, 0), (8, 500, 80, 25), 1)
             for i in range(0, 22):
                 pygame.draw.rect(surf, (i*10, i*10, i*10), (100+(i*35%665),500+(35*((i*35)/665)),32,32), 0)
-        elif self.editor.mode == 'doodad':
+        elif self.editor.mode == DOODAD:
             pygame.draw.rect(surf, (255, 255, 0), (500, 490, 128, 128), 1)
             pygame.draw.rect(surf, (255, 255, 0), (8, 525, 80, 25), 1)
-        elif self.editor.mode == 'entity':
+        elif self.editor.mode == ENTITY:
             pygame.draw.rect(surf, (255, 255, 0), (290, 490, 128, 128), 1)
             pygame.draw.rect(surf, (255, 255, 0), (8, 550, 80, 25), 1)
         
@@ -53,7 +85,39 @@ class Menu(Window):
         surf.blit(self.editor.font.render("ground", True, (255, 255, 255)), (10, 500))
         surf.blit(self.editor.font.render("doodad", True, (255, 255, 255)), (10, 525))
         surf.blit(self.editor.font.render("entity", True, (255, 255, 255)), (10, 550))
+
+class MapView(Window):
     
+    def __init__(self, editor, x, y, w, h, background_color):
+        Window.__init__(self, x, y, w, h, background_color)
+        self.editor = editor
+    
+    def on_event(self, event):
+        pass
+    
+    def draw(self, surf):
+        surf.fill(Color(0, 0, 0, 255))
+        for yy in range(0, self.editor.MAX_Y):
+            for xx in range(0, self.editor.MAX_X):
+                # GROUND
+                g = self.editor.ground[yy+self.editor.Y][xx+self.editor.X]
+                if g == 0:
+                    pygame.draw.rect(surf, (0, 0, 255), (xx*32, yy*32, 32, 32), 0)
+                elif g == 1:
+                    pygame.draw.rect(surf, (255, 0, 0), (xx*32, yy*32, 32, 32), 0)
+                # DOODAD
+                r = self.editor.doodad[yy+self.editor.Y][xx+self.editor.X]
+                if r > 0:
+                    pygame.draw.rect(surf, Color(255, 0, 0, 128), (xx*32, yy*32, 32, 32), 1)
+                    surf.blit(self.editor.content_img[r], (xx*32+self.editor.content[r][1],yy*32+self.editor.content[r][2]))
+                else:
+                    pygame.draw.rect(surf, Color(0, 255, 0, 128), (xx*32, yy*32, 32, 32), 1)
+                # ENTITY
+                e = self.editor.entity[yy+self.editor.Y][xx+self.editor.X]
+                if e != 0:
+                    pygame.draw.circle(surf, (255, 255, 0), (xx*32+16, yy*32+16), 16, 0)
+        
+
 class Editor:
     
     def __init__(self):
@@ -100,10 +164,10 @@ class Editor:
         ]
         self.ground_content = 1
         self.doodad_content = 1
-        self.MAX_GROUND_CONTENT = 1
-        self.MAX_DOODAD_CONTENT = 3
+        self.MAX_GROUND_CONTENT = 2
+        self.MAX_DOODAD_CONTENT = 4
         
-        self.mode = 'ground'
+        self.mode = GROUND
         
         self.content_img = []
         for c in self.content:
@@ -126,7 +190,8 @@ class Editor:
         self.font = pygame.font.SysFont(pygame.font.get_default_font(), 32)
         
         self.menu = Menu(self, 0, 32*self.MAX_Y, 32*self.MAX_X, 100, (255, 0, 0))
-        
+        self.view = MapView(self, 0, 0, 32*self.MAX_X, 32*self.MAX_Y, (0, 0, 255))
+    
     def build(self):
         # ia pour "faire des salles"
         size_x = random.randint(3,10)
@@ -162,6 +227,8 @@ class Editor:
                     elif event.type == MOUSEBUTTONUP:
                         if event.button == 1:
                             self.apply = False
+                            if self.mode == SELECT:
+                                print 'select'
                         elif event.button == 3:
                             print 'line=', my32, 'column=', mx32, self.Y, self.X
                             if mx32 > 0 and mx32 < self.MAP_X and my32 > 0 and my32 < self.MAP_Y: print self.doodad[my32][mx32]
@@ -178,16 +245,12 @@ class Editor:
                 elif event.key == K_LEFT: self.left = False
                 elif event.key == K_RIGHT: self.right = False
                 elif event.key == K_SPACE:
-                    if self.mode == 'doodad':
-                        self.doodad_content += 1
-                        if self.doodad_content > self.MAX_DOODAD_CONTENT: self.doodad_content = 0
-                    elif self.mode == 'ground':
-                        self.ground_content += 1
-                        if self.ground_content > self.MAX_GROUND_CONTENT: self.ground_content = 0
+                    if self.mode == DOODAD:
+                        self.doodad_content = (self.doodad_content + 1) % self.MAX_DOODAD_CONTENT
+                    elif self.mode == GROUND:
+                        self.ground_content = (self.ground_content + 1) % self.MAX_GROUND_CONTENT
                 elif event.key == K_m:
-                    if self.mode == 'doodad': self.mode = 'ground'
-                    elif self.mode == 'ground': self.mode = 'entity'
-                    elif self.mode == 'entity': self.mode = 'doodad'
+                    self.mode = (self.mode + 1) % MODE_MAX
                     print self.mode
         
         #---------------------------------------------------------------
@@ -210,48 +273,16 @@ class Editor:
             print self.X, self.Y
         #if left or right or down or up: print X, Y
         if self.apply:
-            if self.mode == 'doodad':
+            if self.mode == DOODAD:
                 if mx32 >= 0 and mx32 < self.MAP_X and my32 >= 0 and my32 < self.MAP_Y: self.doodad[my32][mx32] = self.doodad_content
                 print 'apply=', self.doodad_content
-            elif self.mode == 'ground':
+            elif self.mode == GROUND:
                 if mx32 >= 0 and mx32 < self.MAP_X and my32 >= 0 and my32 < self.MAP_Y: self.ground[my32][mx32] = self.ground_content
-            elif self.mode == 'entity':
+            elif self.mode == ENTITY:
                 if mx32 >= 0 and mx32 < self.MAP_X and my32 >= 0 and my32 < self.MAP_Y: self.entity[my32][mx32] = Entity()
             
     def draw(self):
-        #---------------------------------------------------------------
-        # Draw
-        self.screen.fill(Color(0, 0, 0, 255))
-        for yy in range(0, self.MAX_Y):
-            for xx in range(0, self.MAX_X):
-                #try:
-                # GROUND
-                g = self.ground[yy+self.Y][xx+self.X]
-                if g == 0:
-                    pygame.draw.rect(self.screen, (0, 0, 255), (xx*32, yy*32, 32, 32), 0)
-                elif g == 1:
-                    pygame.draw.rect(self.screen, (255, 0, 0), (xx*32, yy*32, 32, 32), 0)
-                # DOODAD
-                r = self.doodad[yy+self.Y][xx+self.X]
-                #except:
-                #    print 'y=', Y, yy+Y, 'x=', X, xx+X
-                if r > 0:
-                    pygame.draw.rect(self.screen, Color(255, 0, 0, 128), (xx*32, yy*32, 32, 32), 1)
-                    self.screen.blit(self.content_img[r], (xx*32+self.content[r][1],yy*32+self.content[r][2]))
-                else:
-                    pygame.draw.rect(self.screen, Color(0, 255, 0, 128), (xx*32, yy*32, 32, 32), 1)
-                # ENTITY
-                e = self.entity[yy+self.Y][xx+self.X]
-                if e != 0:
-                    pygame.draw.circle(self.screen, (255, 255, 0), (xx*32+16, yy*32+16), 16, 0)
-        # indicateurs
-        self.screen.blit(self.content_img[self.doodad_content], (500, 450))
-        if self.ground_content == 0:
-            pygame.draw.rect(self.screen, (0, 0, 255), (400, 500, 32, 32), 0)
-        elif self.ground_content == 1:
-            pygame.draw.rect(self.screen, (255, 0, 0), (400, 500, 32, 32), 0)
-        pygame.draw.circle(self.screen, (255, 255, 0), (300+16, 500+16), 16, 0)
-        
+        self.view.draw(self.screen)
         self.menu.draw(self.screen)
         
         pygame.display.flip()
