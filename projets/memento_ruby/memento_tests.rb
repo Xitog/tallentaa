f9237c15e768696a63a2385ec20cd52a19ebd6c7
Xitog <damien.gouteux@gmail.com>
2015-11-25 23:50:14 +0100

    Memento Ruby Loops + If
    - Loops
    - If

--- projets/memento_ruby/memento_tests.rb
+++ projets/memento_ruby/memento_tests.rb
@@ -11,3 +11,17 @@ end
 a = Pipo.new("abc")
 b = Pipo.new("cde")
 puts (a+b).a
+
+for i in 1..3 # lance 3 fois un dé
+  de = Random.new.rand(1..6)
+  puts i, de
+  #retry if de == 1 # recommence tout si on tombe sur 1
+  redo if de < 4 # recommence l'itération si on tombe sur 2 ou 3
+  puts "un lancer validé"
+end
+
+a = 5
+begin
+    puts "hello"
+    puts "world"
+end if a != 5
