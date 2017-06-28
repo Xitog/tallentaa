f9130ed4e467d907683c8943e5223970afeab544
Xitog <damien.gouteux@gmail.com>
2017-06-28 23:34:14 +0200

    [Tkinter] Fusion de Forge et Tree

--- python/TkinterForge.py
+++ python/TkinterForge.py
@@ -1,87 +1,6 @@
-from tkinter import *
-from tkinter import filedialog
-from tkinter import messagebox   
-#from tkinter.filedialog import *
-
-# global : pour changer ou déclarer in a function
-
-# 00h40 : Tkinter relit les fichiers... mais je croyais déjà l'avoir fait !!! Chez mes parents sans save ???
-# http://stackoverflow.com/questions/8384737/extract-file-name-from-path-no-matter-what-the-os-path-format os.path.basename
-# http://stackoverflow.com/questions/2395431/using-tkinter-in-python-to-edit-the-title-bar root.vm_title
-# http://stackoverflow.com/questions/5322027/how-to-erase-everything-from-the-tkinter-text-widget txt.delete("1.0", END)
-# http://effbot.org/tkinterbook/tkinter-standard-dialogs.htm
-# 045 : fin PC
-
-# [ok] charger
-# sauvegarder
-# colorisation du format log
-# colorisation du format lua
-# undo/redo
-
-mode = 'TEXT'
-
-def clear():
-    root.wm_title("* New")
-    txt.delete("1.0", END)
-
-def load(filename : str):
-    txt.delete("1.0", END)
-    f = open(filename, mode='r')
-    content = f.read()
-    f.close()
-    clear()
-    txt.insert("1.0", content)
-    root.wm_title(filename)
-
-def save(filename : str):
-    f = open(filename, mode='w')
-    content = txt.get(1.0,END)
-    f.write(content)
-    f.close()
-    root.wm_title(filename)
 
 def callback():
     print("called the callback!")
-
-def menu_about():
-    messagebox.showinfo(
-        "About",
-        "Jyx - Damien Gouteux, 2017. Made with ❤"
-    )
-
-def menu_new():
-    clear()
-
-def menu_exit():
-    root.destroy()
-    #root.quit()
-    #exit(0)
-
-def menu_open():
-    """Returns an opened file in read mode."""
-    options = {}
-    options['defaultextension'] = '.txt'
-    options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
-    options['initialdir'] = 'C:\\'
-    options['initialfile'] = 'myfile.txt'
-    options['parent'] = root
-    options['title'] = 'This is a title'
-    load(filedialog.askopenfilename(**options))
-
-def menu_save():
-    options = {}
-    #options['defaultextension'] = '.txt'
-    options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
-    options['initialdir'] = 'C:\\'
-    options['initialfile'] = 'myfile.txt'
-    options['parent'] = root
-    options['title'] = 'This is a title'
-    filename = filedialog.asksaveasfilename(**options)
-    if filename:
-        save(filename)
-    
-root = Tk()
-root.wm_title("* New")
 
 if mode == 'CANVAS':
     try:
@@ -92,26 +11,6 @@ if mode == 'CANVAS':
 
 # root.minsize(root.winfo_screenwidth(), root.winfo_screenheight())
 # print(root.wm_maxsize())
-
-#------------------------------------------------
-# Menu
-#------------------------------------------------
-
-menu = Menu(root)
-root.config(menu=menu)
-
-filemenu = Menu(menu, tearoff=0)
-menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New", command=menu_new)
-filemenu.add_command(label="Open...", command=menu_open)
-filemenu.add_command(label="Save As...", command=menu_save)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=menu_exit)
-
-helpmenu = Menu(menu, tearoff=0)
-menu.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="About...", command=menu_about)
-
 # toplevel = root.winfo_toplevel()
 
 #------------------------------------------------
@@ -131,21 +30,8 @@ if mode == 'TEXT':
     frame.grid_columnconfigure(0, weight=1)
     
     txt = Text(frame, borderwidth=3, relief="sunken")
-    txt.config(font=("consolas", 12), undo=True, wrap='word')
+    #config
     txt.grid(row=0, column=0, sticky="nsew", padx=2, pady=2)
-    
-    def tab(arg):
-        print("tab pressed")
-        txt.insert(INSERT, " " * 4)
-        return 'break' # Prevent normal behavior
-    
-    txt.bind("<Tab>", tab)
-    
-    scrollbar = Scrollbar(frame, command=txt.yview)
-    scrollbar.grid(row=0, column=1, sticky='nsew')
-    txt['yscrollcommand'] = scrollbar.set
-    
-
     
 #------------------------------------------------
 # Canvas
@@ -161,14 +47,4 @@ elif mode == 'CANVAS':
 
     w.create_rectangle(50, 25, 150, 75, fill="blue")
 
-#------------------------------------------------
-# Status bar
-#------------------------------------------------
-
-status_bar = Label(root, bd=1, relief=SUNKEN, anchor=W)
-status_bar.config(text="Hello!")
-#status_bar.update_idletasks()
-status_bar.pack(side=BOTTOM, fill=X)
-
-mainloop()
 
--- python/TkinterTree.py
+++ python/TkinterTree.py
@@ -2,71 +2,48 @@
 
 import tkinter # as tk
 from tkinter import ttk # not found in 2.7.11 but found in 3.5.1 :-)
+from tkinter import filedialog
 from tkinter import messagebox   
 
-def menu_about():
-    messagebox.showinfo(
-        "About",
-        "Jyx - Damien Gouteux, 2017. Made with ❤"
-    )
-
-def menu_new():
-    clear()
-
-def menu_exit():
-    root.destroy()
-    #root.quit()
-    #exit(0)
-
-def menu_open():
-    """Returns an opened file in read mode."""
-    options = {}
-    options['defaultextension'] = '.txt'
-    options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
-    options['initialdir'] = 'C:\\'
-    options['initialfile'] = 'myfile.txt'
-    options['parent'] = root
-    options['title'] = 'This is a title'
-    load(filedialog.askopenfilename(**options))
-
-def menu_save():
-    options = {}
-    #options['defaultextension'] = '.txt'
-    options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
-    options['initialdir'] = 'C:\\'
-    options['initialfile'] = 'myfile.txt'
-    options['parent'] = root
-    options['title'] = 'This is a title'
-    filename = filedialog.asksaveasfilename(**options)
-    if filename:
-        save(filename)
 
 class Application():
     
     def __init__(self):
         # root widget, an ordinary window
         self.root = tkinter.Tk()
-        self.root.wm_title("Forge")
+        self.set_title()
         self.root.iconbitmap(r'icons\iconyellowcube16x19_F5i_icon.ico')
         self.root.minsize(width=800, height=600)
+        self.update()
         self.make_menu()
         self.make_status_bar()
         self.frame = MyFrame(self.root)
+        self.text = self.frame.text
+    
+    def update(self):
+        print("hello")
+        self.root.after(1000, self.update)
+
+    def set_title(self, filename=None):
+        if filename is None:
+            self.root.wm_title("Forge - New *")
+        else:
+            self.root.wm_title("Forge - " + filename)
     
     def make_menu(self):
         self.menu = tkinter.Menu(self.root)
         self.root.config(menu=self.menu)
         self.filemenu = tkinter.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="File", menu=self.filemenu)
-        self.filemenu.add_command(label="New", command=menu_new)
-        self.filemenu.add_command(label="Open...", command=menu_open)
-        self.filemenu.add_command(label="Save As...", command=menu_save)
+        self.filemenu.add_command(label="New", command=self.menu_new)
+        self.filemenu.add_command(label="Open...", command=self.menu_open)
+        self.filemenu.add_command(label="Save As...", command=self.menu_save)
         self.filemenu.add_separator()
-        self.filemenu.add_command(label="Exit", command=menu_exit)
+        self.filemenu.add_command(label="Exit", command=self.menu_exit)
 
         self.helpmenu = tkinter.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="Help", menu=self.helpmenu)
-        self.helpmenu.add_command(label="About...", command=menu_about)
+        self.helpmenu.add_command(label="About...", command=self.menu_about)
     
     def make_status_bar(self):
         self.status_bar = tkinter.Label(self.root, bd=1, relief=tkinter.SUNKEN, anchor=tkinter.W)
@@ -77,6 +54,72 @@ class Application():
     def run(self):
         self.frame.mainloop()
         # root.destroy()
+    
+    #-------------------------------------------------------
+    # Menu functions
+    #-------------------------------------------------------
+    
+    def menu_about(self):
+        messagebox.showinfo("About", "Jyx - Damien Gouteux, 2017. Made with ❤")
+
+    def menu_exit(self):
+        self.root.destroy()
+        #root.quit()
+        #exit(0)
+
+    def menu_new(self):
+        self.new()
+    
+    def menu_open(self):
+        """Returns an opened file in read mode."""
+        options = {}
+        options['defaultextension'] = '.txt'
+        options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
+        options['initialdir'] = 'C:\\'
+        options['initialfile'] = 'myfile.txt'
+        options['parent'] = self.root
+        options['title'] = 'This is a title'
+        self.load(filedialog.askopenfilename(**options))
+
+    def menu_save(self):
+        options = {}
+        #options['defaultextension'] = '.txt'
+        options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
+        options['initialdir'] = 'C:\\'
+        options['initialfile'] = 'myfile.txt'
+        options['parent'] = self.root
+        options['title'] = 'This is a title'
+        filename = filedialog.asksaveasfilename(**options)
+        if filename:
+            self.save(filename)
+
+    #-------------------------------------------------------
+    # Text functions
+    #-------------------------------------------------------
+
+    def new(self):
+        self.clear()
+        self.set_title()
+    
+    def clear(self):
+        self.text.delete("1.0", tkinter.END)
+
+    def load(self, filename : str):
+        self.text.delete("1.0", tkinter.END)
+        f = open(filename, mode='r')
+        content = f.read()
+        f.close()
+        self.clear()
+        self.text.insert("1.0", content)
+        self.set_title(filename)
+
+    def save(self, filename : str):
+        f = open(filename, mode='w')
+        content = self.text.get(1.0, tkinter.END)
+        f.write(content)
+        f.close()
+        self.set_title(filename)
+
 
 class MyFrame(tkinter.Frame):
     """ Extend a Frame, a global container"""
@@ -110,7 +153,7 @@ class MyFrame(tkinter.Frame):
         treeview.insert("sub2", 0, text=" 3-1 Entry", image=self.graphics['iconyellow'])
         
         treeview.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=tkinter.YES)
-    
+        
     def make_buttons(self: tkinter.Frame):
         # this label widget is a child of the frame widget
         label = tkinter.Label(self, text="Hello, world!")
@@ -128,15 +171,57 @@ class MyFrame(tkinter.Frame):
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.hello
         self.hi_there.pack(side="bottom")
+
+    def make_text(self: tkinter.Frame): # with pack: scrollbar too big when expanding
+        self.text_frame = tkinter.Frame(self)
+        self.text_frame.pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=tkinter.YES)
         
+        self.text = tkinter.Text(self.text_frame)
+        self.text.config(font=("consolas", 12), undo=True, wrap='word')
+        self.text.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=tkinter.YES)
+        
+        # Tabs
+        def tab(arg):
+            print("tab pressed")
+            self.text.insert(tkinter.INSERT, " " * 4)
+            return 'break' # Prevent normal behavior
+        self.text.bind("<Tab>", tab)
+        
+        # Scrollbar
+        self.scrollbar = tkinter.Scrollbar(self.text_frame, command=self.text.yview)
+        self.scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=tkinter.YES)
+        self.text['yscrollcommand'] = self.scrollbar.set
+
+    def make_text(self: tkinter.Frame): # with grid: ok :-)
+        self.text_frame = tkinter.Frame(self, bd=2, relief=tkinter.SUNKEN)
+
+        self.text_frame.grid_rowconfigure(0, weight=1)
+        self.text_frame.grid_columnconfigure(0, weight=1)
+
+        #self.xscrollbar = tkinter.Scrollbar(self.text_frame, orient=tkinter.HORIZONTAL)
+        #self.xscrollbar.grid(row=1, column=0, sticky=tkinter.E+tkinter.W)
+
+        self.yscrollbar = tkinter.Scrollbar(self.text_frame)
+        self.yscrollbar.grid(row=0, column=1, sticky=tkinter.N+tkinter.S)
+
+        self.text = tkinter.Text(self.text_frame, wrap=tkinter.NONE, bd=0,
+                    #xscrollcommand=self.xscrollbar.set,
+                    yscrollcommand=self.yscrollbar.set)
+        self.text.config(font=("consolas", 12), undo=True, wrap='word')
+        
+        self.text.grid(row=0, column=0, sticky=tkinter.N+tkinter.S+tkinter.E+tkinter.W)
+
+        #self.xscrollbar.config(command=self.text.xview)
+        self.yscrollbar.config(command=self.text.yview)
+
+        self.text_frame.pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=tkinter.YES)
+    
     def build(self):
         self.make_tree()
         # self.make_buttons()
-        text = tkinter.Text(self)
-        text.pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=tkinter.YES)
-        
-    @staticmethod
-    def hello():
+        self.make_text()
+    
+    def hello(self):
         print("hello!")
 
 if __name__ == "__main__":
