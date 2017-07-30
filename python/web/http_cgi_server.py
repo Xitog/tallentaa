59d83d95bf2b442cd836cf3bf0f831cc998ca07a
Xitog <damien.gouteux@gmail.com>
2017-07-30 16:00:28 +0200

    [Python][web] Server can serve now images, js, css
    Added open binary for serving image files.
    Redesign between serve, serve_default, serve_file.

--- python/web/http_cgi_server.py
+++ python/web/http_cgi_server.py
@@ -10,6 +10,51 @@ PORT = 8013
 
 #class HTTPHandler(http.server.SimpleHTTPRequestHandler):
 class HTTPHandler(CGIHTTPRequestHandler):
+
+    def serve(self, content, content_type):
+        self.send_response(200)
+        self.send_header("Content-type", content_type)
+        self.send_header("Content-Length", str(content))
+        self.end_headers()
+        self.wfile.write(content)
+        
+    def serve_default(self):
+        "Basic HTML return"
+        content = "<html><body><h1>Hello World</h1></body></html>"
+        typ = 'text/html'
+        encoding = sys.getfilesystemencoding()
+        content = content.encode(encoding)
+        content_type = f"{typ}; charset={encoding}"
+        self.serve(content, content_type)
+
+    def serve_file(self, target):
+        ext = os.path.splitext(target)[1]
+        if ext in ['.html', '.css', '.js', '.htm']:
+            if ext == '.html':
+                typ = 'text/html'
+            elif ext == '.css':
+                typ = 'text/css'
+            elif ext == '.js':
+                typ = 'application/javascript'
+            f = open(target, 'r')
+            content = f.read()
+            f.close()
+            encoding = sys.getfilesystemencoding() # 'utf-8'
+            content = content.encode(encoding)
+            content_type = f"{typ}; charset={encoding}"
+            self.serve(content, content_type)
+        elif ext in ['.jpeg', '.jpg', '.png', '.gif', '.ico']:
+            if ext == '.png':
+                typ = 'image/png'
+            elif ext == '.gif':
+                typ = 'image/gif'
+            elif ext == '.ico':
+                typ = 'image/x-icon'
+            f = open(target, 'rb')
+            content = f.read()
+            f.close()
+            content_type = f"{typ}"
+            self.serve(content, content_type)
     
     def do_GET(self):
         current_dir = os.getcwd()
@@ -23,20 +68,12 @@ class HTTPHandler(CGIHTTPRequestHandler):
             if os.path.splitext(target)[1] == '.py':
                 CGIHTTPRequestHandler.do_GET(self)
                 return
+            # no CGI
             else:
-                f = open(target, 'r')
-                content = f.read()
-                f.close()
-        # Basic HTML return
+                self.serve_file(target)
+        # no file
         else:
-            content = "<html><body><h1>Hello World</h1></body></html>"
-        content = content.encode('utf-8')
-        self.send_response(200)
-        encoding = sys.getfilesystemencoding()
-        self.send_header("Content-type", "text/html; charset=%s" % encoding)
-        self.send_header("Content-Length", str(content))
-        self.end_headers()
-        self.wfile.write(content)
+            self.serve_default()
 
 handler = HTTPHandler
 handler.cgi_directories = ["/"]
