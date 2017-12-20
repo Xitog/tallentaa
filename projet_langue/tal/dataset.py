3dea5b9e83e00d46f09c7ddc3deef529db0476ad
Xitog <damien.gouteux@gmail.com>
2017-12-21 00:09:39 +0100

    [TAL] Premiers coups de sonde dans le corpus

--- projet_langue/tal/dataset.py
+++ projet_langue/tal/dataset.py
@@ -9,12 +9,24 @@ with open('titres-FR-2017-10-20-01.tsv', mode='r', encoding='utf-8') as datafile
     #
     
     all_fields = {}
-    title_with_column = 0
+    #title_with_column = 0
+    #title_with_question = 0
+    look_for = {'!' : 0, ':' : 0, '?' : 0}
+    lengths = []
     
     for line in lines:
         id, title, typ, year, nb_authors, field, subfields = line.split('\t')
-        if ':' in title:
-            title_with_column += 1
+        # length
+        lengths.append(len(title.split(' ')))
+        # elements
+        for k in look_for:
+            if k in title:
+                look_for[k] += 1
+        #if ':' in title:
+        #    title_with_column += 1
+        #if '?' in title:
+        #    title_with_question += 1
+        # fields
         if field not in all_fields:
             all_fields[field] = 1
         else:
@@ -23,10 +35,18 @@ with open('titres-FR-2017-10-20-01.tsv', mode='r', encoding='utf-8') as datafile
     #
     # Counts
     #
-    
+    moyenne = 0
+    for i in lengths:
+        moyenne += i
+    moyenne = moyenne / len(lengths)
     print()
     print("Number of titles:", len(lines)) # 146 603
-    print("Number of titles with ':':", title_with_column, '(', round((title_with_column / len(lines))*100, 2), '%)')
+    #print("Number of titles with ':':", title_with_column, '(', round((title_with_column / len(lines))*100, 2), '%)')
+    #print("Number of titles with '?':", title_with_question, '(', round((title_with_question / len(lines))*100, 2), '%)')
+    for k in look_for:
+        print("Number of titles with", k, look_for[k], '(', round((look_for[k] / len(lines)) * 100, 2), '%)')
+    print("Longueur moyenne :", int(moyenne))
+    
     print()
     
     #
