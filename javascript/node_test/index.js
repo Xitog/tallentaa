1506106d757f392ce03e11b9d4fce92eab948599
Xitog <Xitog@users.noreply.github.com>
2018-12-18 15:38:59 +0100

    Tests avec node

--- javascript/node_test/index.js
+++ javascript/node_test/index.js
@@ -1,7 +1,31 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
+    let query = url.parse(req.url, true).query;
+    let produce = 'text';
+    if ('format' in query) {
+        produce = query['format'];
+    }
+    if (produce == 'text') {
+        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+        res.write('Hello Http\n');
+        res.write(JSON.stringify(query) + '\n');
+        let contents = fs.readFileSync('data.txt', 'utf8');
+        res.write(contents + '\n');
+        res.end('Goodbye');
+    } else if (produce == 'html') {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.write('<h1>Hello Http</h1><br>');
+        res.write('<pre>' + JSON.stringify(query) + '</pre><br>');
+        let contents = fs.readFileSync('data.txt', 'utf8');
+        res.write('<p>' + contents + '</p>');
+        res.end('<p>Goodbye</p>');
+    } else if (produce == 'json') {
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+        res.write(JSON.stringify(query, null, 4));
+        res.end();
+    }
 });
 server.listen(8080);
