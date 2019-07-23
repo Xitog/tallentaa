c7e640f82dbcdef8c584363d02196b3635bc5f38
Xitog <Xitog@users.noreply.github.com>
2019-07-23 15:41:55 +0000

    PyMatrix : save to image through pygame (and to Excel added friday)

--- python/matrix.py
+++ python/matrix.py
@@ -1,7 +1,13 @@
 try:
     import openpyxl
+    from openpyxl.worksheet.write_only import WriteOnlyCell
+    from openpyxl.styles import PatternFill
 except ModuleNotFoundError:
     openpyxl = None
+try:
+    import pygame
+except ModuleNotFoundError:
+    pygame = None
 
 # x = col
 # y = row
@@ -32,25 +38,40 @@ class Matrix:
             for x in range(0, self.width):
                 print(self.data[x][y], '', end='')
             print()
-
+    
     def to_excel(self, title="output.xlsx"):
         if openpyxl is None:
             raise Exception("Module openpyxl not available.")
         else:
             wb = openpyxl.Workbook(write_only=True)
             ws = wb.create_sheet("One")
+            ws.sheet_format.defaultColWidth = 3.0
+            ws.defaultRowHeight = 4.0
+            fill = PatternFill("solid", fgColor="FF0000")
             for y in range(0, self.height):
                 row = []
                 for x in range(0, self.width):
-                    row.append(self.data[x][y])
+                    cell = WriteOnlyCell(ws, value=self.data[x][y])
+                    cell.fill = fill
+                    row.append(cell)
+                    #row.append(self.data[x][y])
                 ws.append(row)
             wb.save(title)
 
-    def to_image(self):
+    def to_image(self, title="output.png"):
         if pygame is None:
             raise Exception("Module pygame not available.")
         else:
-            pass
+            #pygame.init()
+            pygame.font.init()
+            font = pygame.font.SysFont("arial", 16)
+            surf = pygame.Surface((32 * self.width, 32 * self.height))
+            for y in range(0, self.height):
+                for x in range(0, self.width):
+                    pygame.draw.rect(surf, (255, 0, 0), (x*32, y*32, 32, 32), 2)
+                    tex = font.render(str(self.data[x][y]), True, (255, 255, 255))
+                    surf.blit(tex, (x*32+10, y*32+8))
+            pygame.image.save(surf, title)
     
     def to_screen(self):
         pass
@@ -64,5 +85,6 @@ if __name__ == '__main__':
     m.set(0, 9, 4)
     m.print()
     m.to_excel()
+    m.to_image("zorba.png")
     m.fill(5)
     m.print()
