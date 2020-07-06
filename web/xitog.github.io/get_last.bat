e4f1d7f7266fd1835fbaa86daf0a14712d1e8370
Xitog <damien.gouteux@gmail.com>
2020-07-07 00:11:20 +0200

    [Transition] Article : je continue

--- web/xitog.github.io/get_last.bat
+++ web/xitog.github.io/get_last.bat
@@ -3,4 +3,3 @@ if not exist hamill (
 )
 copy ..\..\projets\hamill\hamill\__init__.py  hamill\__init__.py  /B /Y
 copy ..\..\projets\hamill\hamill\hamill.py    hamill\hamill.py    /B /Y
-copy ..\..\projets\hamill\hamill\tokenizer.py hamill\tokenizer.py /B /Y
