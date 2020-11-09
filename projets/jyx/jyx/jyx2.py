931676b7f4338b824b77a14a39c31489731c757d
Xitog <damien.gouteux@gmail.com>
2020-11-09 22:02:38 +0100

    [Jyx2] Division App / Text + Menu

--- projets/jyx/jyx/jyx2.py
+++ projets/jyx/jyx/jyx2.py
@@ -1,83 +1,187 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk
 
 class Jyx:
 
     def __init__(self):
-        self.root = tkinter.Tk()
+        self.root = tk.Tk()
         self.root.protocol('WM_DELETE_WINDOW', self.exit)
+        self.text = JyxText(self)
+        self.menu = JyxMenu(self)
+        self.root.config(menu=self.menu)
         self.update_title()
+        self.root.mainloop()
+
+    def get_root(self):
+        return self.root
+
+    def update_title(self):
+        dirty = ' *' if self.text.dirty else ''
+        self.root.wm_title('Jyx' + dirty)
+
+    def exit(self, event=None):
+        self.root.destroy()
+
+
+class JyxMenu(tk.Menu):
+
+    def __init__(self, parent):
+        tk.Menu.__init__(self, parent.get_root())
+        self.parent = parent
+        self.filemenu = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="File", menu=self.filemenu)
+
+
+class JyxText(tk.Text):
+
+    def __init__(self, parent):
+        tk.Text.__init__(self, parent.get_root())
+        self.parent = parent
         self.dirty = False
         
-        frame = tkinter.Frame(self.root, bd=2, relief=tkinter.SUNKEN)
+        frame = ttk.Frame(self.parent.get_root()) #, bd=2, relief=tk.SUNKEN)
         # To make the element at 0,0 grows with the window
         frame.grid_rowconfigure(0, weight=1)
         frame.grid_columnconfigure(0, weight=1)
 
-        yscrollbar = tkinter.Scrollbar(frame)
-        yscrollbar.grid(row=0, column=1, sticky=tkinter.N+tkinter.S)
-        text = tkinter.Text(frame, wrap=tkinter.NONE, bd=0,
+        yscrollbar = ttk.Scrollbar(frame)
+        yscrollbar.grid(row=0, column=1, sticky=tk.N+tk.S)
+        self.text = tk.Text(frame, wrap=tk.NONE, bd=0,
                             yscrollcommand=yscrollbar.set)
-        text.config(font=('consolas', 12), undo=True, wrap='word')
-        text.grid(row=0, column=0, sticky=tkinter.N+tkinter.S+tkinter.E+tkinter.W)
-        text.bind('<<Paste>>', self.paste)
-        text.bind('<<Cut>>', self.cut)
-        text.bind('<<Copy>>', self.copy)
-        text.bind('<KeyPress>', self.update_text)
-        #text.bind('<KeyRelease>', self.update_text)
+        self.text.config(font=('consolas', 12), undo=True, wrap='word')
+        self.text.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.text.bind('<<Paste>>', self.paste)
+        self.text.bind('<<Cut>>', self.cut)
+        self.text.bind('<<Copy>>', self.copy)
+        self.text.bind('<KeyPress>', self.update_text_before)
+        self.text.bind('<KeyRelease>', self.update_text_after)
 
-        text.tag_config("a", foreground="blue", underline=1)
+        self.text.tag_config("a", foreground="blue", underline=1)
+
+        self.start = None
         
-        frame.pack(fill=tkinter.BOTH, expand=tkinter.YES)
+        frame.pack(fill=tk.BOTH, expand=tk.YES)
+        self.text.focus_force()
 
+    #
+    # Handling of state
+    #
+    def write(self, content, at=tk.INSERT):
+        self.text.insert(at, content)
+        self.dirty = True
+        self.parent.update_title()
+
+    def delete(self, first, last=None):
+        self.text.delete(first, last)
+        self.dirty = True
+        self.parent.update_title()
+
+    #
+    # Handling of selection: deleting, getting, selecting and unselecting
+    #
+    def selection_delete(self):
+        self.delete(tk.SEL_FIRST, tk.SEL_LAST)
+        self.selection_clear()
+
+    def selection_get(self):
+        return self.text.get(tk.SEL_FIRST, tk.SEL_LAST)
+
+    def selection_set(self, index1, index2):
+        if self.text.compare(index1, '<', index2):
+            self.text.tag_add('sel', f'{index1}', f'{index2}')
+        else:
+            self.text.tag_add('sel', f'{index2}', f'{index1}')
+
+    def selection_clear(self):
+        self.text.tag_remove('sel', '1.0', tk.END)
+
+    #
+    # Basic functions
+    #
     def paste(self, event):
         content =  self.root.clipboard_get()
-        event.widget.insert(tkinter.INSERT, content)
+        event.widget.insert(tk.INSERT, content)
         return 'break'
 
     def cut(self, event):
-        self.root.clipboard_clear()
-        self.root.clipboard_append(event.widget.get(tkinter.SEL_FIRST, tkinter.SEL_LAST))        
-        event.widget.delete(tkinter.SEL_FIRST, tkinter.SEL_LAST)
+        self.copy(event)
+        self.selection_delete()
         return 'break'
 
     def copy(self, event):
         self.root.clipboard_clear()
-        self.root.clipboard_append(event.widget.get(tkinter.SEL_FIRST, tkinter.SEL_LAST))
+        self.root.clipboard_append(self.selection_get())
         return 'break'
 
-    def update_title(self):
-         self.root.wm_title('Jyx')
-
-    def update_text(self, event):
-        #print('updated!')
-        insert = event.widget.index(tkinter.INSERT)
-        if event.keycode == 37: # left
-            event.widget.mark_set(tkinter.INSERT, f'{insert}-1c')
+    def update_text_before(self, event):
+        text = event.widget
+        Shift = 0x0001
+        if event.keysym in ['Left', 'Right', 'Up', 'Down', 'End', 'Home']: # 37, 39
+            codes = {
+                'Left': 'insert-1c',
+                'Right': 'insert+1c',
+                'Up': 'insert-1l',
+                'Down': 'insert+1l',
+                'End': 'insert lineend',
+                'Home': 'insert linestart'
+            }
+            nex = codes[event.keysym]
+            if len(text.tag_ranges('sel')) == 0:
+                self.start = text.index(tk.INSERT)
+            else:
+                self.selection_clear()
+            if Shift & event.state:
+                self.selection_set(nex, self.start)
+            text.mark_set(tk.INSERT, f'{nex}')
             return 'break'
-        elif event.keycode == 39:
-            event.widget.mark_set(tkinter.INSERT, f'{insert}+1c')
+        elif event.keysym == 'BackSpace':
+            if len(text.tag_ranges('sel')) > 0:
+                self.selection_delete()
+            else:
+                text.mark_set(tk.INSERT, 'insert-1c')
+                self.delete(tk.INSERT)
             return 'break'
-        elif event.keycode == 8:
-            #delete
+        elif event.keysym == 'Delete':
+            if len(text.tag_ranges('sel')) > 0:
+                self.selection_delete()
+            return 'break'
+        elif event.keysym == 'Escape':
+            self.selection_clear()
             return 'break'
         elif event.char == '\r':
             content = '\n'
         elif event.char == '\t':
             content = '    '
-        else:
+        elif event.keysym == 'space':
+            content = ' '
+        elif event.char.isalnum():
+            print(event)
             content = event.char
-        print(event)
-        event.widget.insert(tkinter.INSERT, content)
+        else:
+            print('Unknown:', event)
+            return 'break'
+        if len(text.tag_ranges('sel')) > 0:
+            self.selection_delete()
+        self.write(content)
         c = event.widget.get("insert linestart", "insert lineend")
         print(c)
         if c.startswith('http://'):
-            event.widget.tag_add('a', 'insert linestart', tkinter.INSERT)
+            text.tag_add('a', 'insert linestart', tk.INSERT)
         else:
-            event.widget.tag_remove('a', 'insert linestart', tkinter.INSERT)
+            text.tag_remove('a', 'insert linestart', tk.INSERT)
+        text.see(tk.INSERT)
         return 'break'
-    
-    def exit(self, event=None):
-        self.root.destroy()
+
+    def update_text_after(self, event):
+        #print('h', event)
+        res = event.widget.search('--> ', "insert linestart", "insert lineend")
+        while res != '':
+            print(res)
+            self.delete(res, res + '+4c')
+            self.write(res, '→ ')
+            res = event.widget.search('--> ', res, "insert lineend")
+        return 'break'
+
 
 if __name__ == '__main__':
     Jyx()
