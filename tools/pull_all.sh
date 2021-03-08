a1251b9c166148bd6c7004f584f3f42db03f71c7
Damien Gouteux <damien.gouteux@gmail.com>
2021-03-08 11:02:49 +0100

    8 repo on lx

--- tools/pull_all.sh
+++ tools/pull_all.sh
@@ -1,12 +1,12 @@
 # status:       lx      w8      w10     https                                   ssh
-# ash                   yes             https://github.com/Xitog/ash.git        git@github.com:Xitog/ash.git
+# ash           yes     yes             https://github.com/Xitog/ash.git        git@github.com:Xitog/ash.git
 # dgx           yes     yes             https://github.com/Xitog/dgx.git        git@github.com:Xitog/dgx.git
-# hamill                yes             https://github.com/Xitog/hamill.git     git@github.com:Xitog/hamill.git
+# hamill        yes     yes             https://github.com/Xitog/hamill.git     git@github.com:Xitog/hamill.git
 # jyx           yes     yes             https://github.com/Xitog/jyx.git        git@github.com:Xitog/jyx.git
 # tal                   yes             https://github.com/Xitog/tal.git        git@github.com:Xitog/tal.git
 # tallentaa     yes     yes             https://github.com/Xitog/tallentaa.git  git@github.com:Xitog/tallentaa.git
 # teddy         yes     yes             https://github.com/Xitog/teddy.git      git@github.com:Xitog/teddy.git
-# raycasting            yes             https://github.com/Xitog/raycasting.git git@github.com:Xitog/raycasting.git
+# raycasting    yes     yes             https://github.com/Xitog/raycasting.git git@github.com:Xitog/raycasting.git
 # weyland       yes     yes             https://github.com/Xitog/weyland.git    git@github.com:Xitog/weyland.git
 
 GREEN='\033[1;32m'
