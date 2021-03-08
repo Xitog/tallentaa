ce993868da031dd132fdf2e52bd5b30cb6ba2f39
Damien Gouteux <damien.gouteux@gmail.com>
2021-03-08 10:20:24 +0100

    Pull and status for all repo

--- tools/pull_all.sh
+++ tools/pull_all.sh
@@ -1,28 +1,24 @@
-echo Updating Jyx https://github.com/Xitog/jyx.git
-cd jyx
-pull
-#git pull
-cd ..
+# status:       lx      w8      w10     https                                   ssh
+# ash                   yes             https://github.com/Xitog/ash.git        git@github.com:Xitog/ash.git
+# dgx           yes     yes             https://github.com/Xitog/dgx.git        git@github.com:Xitog/dgx.git
+# hamill                yes             https://github.com/Xitog/hamill.git     git@github.com:Xitog/hamill.git
+# jyx           yes     yes             https://github.com/Xitog/jyx.git        git@github.com:Xitog/jyx.git
+# tal                   yes             https://github.com/Xitog/tal.git        git@github.com:Xitog/tal.git
+# tallentaa     yes     yes             https://github.com/Xitog/tallentaa.git  git@github.com:Xitog/tallentaa.git
+# teddy         yes     yes             https://github.com/Xitog/teddy.git      git@github.com:Xitog/teddy.git
+# raycasting            yes             https://github.com/Xitog/raycasting.git git@github.com:Xitog/raycasting.git
+# weyland       yes     yes             https://github.com/Xitog/weyland.git    git@github.com:Xitog/weyland.git
 
-echo Updating Tallentaa https://github.com/Xitog/tallentaa.git
-cd tallentaa
-pull
-#git pull
-cd ..
-
-echo Updating Dgx https://github.com/Xitog/dgx.git
-cd dgx
-pull
-#git pull
-cd ..
-
-echo Updating Teddy https://github.com/Xitog/teddy
-cd teddy
-pull
-#git pull
-cd ..
-
-echo Updating Old School Raycasting https://github.com/Xitog/old-school-raycasting
-echo skipped
+for file in */ ; do
+    name=${file%*/} 
+    echo ---------------------------------------------------------------
+    echo Updating "$name";
+    echo ---------------------------------------------------------------
+    cd $file;
+    pull;
+    git status;
+    cd ..
+    printf "\n"
+done
 
 echo Fin
