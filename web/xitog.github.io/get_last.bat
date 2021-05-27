a52e4a9a2dedde90e5267adab173aac3575f9de0
Xitog <damien.gouteux@gmail.com>
2021-05-27 10:37:52 +0200

    Moved static hml pages into dgx

--- web/xitog.github.io/get_last.bat
+++
@@ -1,5 +0,0 @@
-if not exist hamill (
-    mkdir hamill
-)
-copy ..\..\projets\hamill\hamill\__init__.py  hamill\__init__.py  /B /Y
-copy ..\..\projets\hamill\hamill\hamill.py    hamill\hamill.py    /B /Y
